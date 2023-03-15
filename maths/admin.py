c70839ebff2ed1d9701277ae1cad03a121fa68eb
deciever5 <marcinkunikowski@gmail.com>
2023-03-15 14:24:13 +0100

    added classes to admin.py in mats and posts

--- maths/admin.py
+++ maths/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# maths/admin.py
+from django.contrib import admin
+from .models import Math, Result
 # Register your models here.
+
+admin.site.register(Math)
+admin.site.register(Result)
