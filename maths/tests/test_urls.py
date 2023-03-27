903d852b43492ddfc23ba342331ab37b3d748d96
deciever5 <marcinkunikowski@gmail.com>
2023-03-27 11:31:39 +0200

    added fixtures and tests to math app

--- maths/tests/test_urls.py
+++ maths/tests/test_urls.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
 from django.urls import resolve
 from django.urls.exceptions import Resolver404
-from dingo.maths.views import add, sub
-
+from dingo.maths.views import math, add, sub, mul, div, maths_list, math_details, results_list
 
 class TestUrls(TestCase):
+    def test_resolution_for_math(self):
+        resolver = resolve('/maths/')
+        self.assertEqual(resolver.func, math)
+
     def test_resolution_for_add(self):
         resolver = resolve('/maths/add/1/2')
         self.assertEqual(resolver.func, add)
@@ -13,6 +16,26 @@ class TestUrls(TestCase):
         resolver = resolve('/maths/sub/1/2')
         self.assertEqual(resolver.func, sub)
 
+    def test_resolution_for_mul(self):
+        resolver = resolve('/maths/mul/1/2')
+        self.assertEqual(resolver.func, mul)
+
+    def test_resolution_for_div(self):
+        resolver = resolve('/maths/div/1/2')
+        self.assertEqual(resolver.func, div)
+
+    def test_resolution_for_maths_list(self):
+        resolver = resolve('/maths/histories/')
+        self.assertEqual(resolver.func, maths_list)
+
+    def test_resolution_for_math_details(self):
+        resolver = resolve('/maths/histories/1')
+        self.assertEqual(resolver.func, math_details)
+
+    def test_resolution_for_results_list(self):
+        resolver = resolve('/maths/results/')
+        self.assertEqual(resolver.func, results_list)
+
     def test_arguments_should_be_integers_or_404(self):
         with self.assertRaises(Resolver404):
-            resolve('maths/sub/a/b')
+            resolve('/maths/sub/a/b')
--- maths/tests/test_views.py
+++ maths/tests/test_views.py
@@ -1,15 +1,54 @@
 from django.test import TestCase, Client
-from dingo.maths.models import Math
-
+from dingo.maths.models import Math, Result
 
 class MathViewsTest(TestCase):
 
     def setUp(self):
-        Math.objects.create(operation="sub", a=20, b=30)
+        result = Result.objects.create(value=10)
+        Math.objects.create(operation="add", a=5, b=5, result=result)
         self.client = Client()
+
+    def test_math_view(self):
+        response = self.client.get("/maths/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Tu będzie matma", response.content.decode())
+
+    def test_add_view(self):
+        response = self.client.get("/maths/add/3/4")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("operacja: +", response.content.decode())
+        self.assertIn("wynik: 7", response.content.decode())
+
+    def test_sub_view(self):
+        response = self.client.get("/maths/sub/7/3")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("operacja: -", response.content.decode())
+        self.assertIn("wynik: 4", response.content.decode())
+
+    def test_mul_view(self):
+        response = self.client.get("/maths/mul/2/3")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("operacja: *", response.content.decode())
+        self.assertIn("wynik: 6", response.content.decode())
+
+    def test_div_view(self):
+        response = self.client.get("/maths/div/8/2")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("operacja: /", response.content.decode())
+        self.assertIn("wynik: 4.0", response.content.decode())
+
+    def test_div_view_zero_division(self):
+        response = self.client.get("/maths/div/8/0")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Error", response.content.decode())
 
     def test_maths_list(self):
         response = self.client.get("/maths/histories/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context["maths"]), 1)
-        self.assertIn('<li><a href="/maths/histories/1">id:1, a=20, b=30, op=sub</a></li>', response.content.decode())
+        self.assertIn('<li><a href="/maths/histories/1">id:1, a=5, b=5, op=add</a></li>', response.content.decode())
+
+    def test_math_details(self):
+        response = self.client.get("/maths/histories/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("id:1, a=5, b=5, op=add", response.content.decode())
